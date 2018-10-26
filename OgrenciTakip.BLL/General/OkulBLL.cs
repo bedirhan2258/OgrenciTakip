3545d0688f7d88306a97ee29fbe2bebbc57da31d
bedirhan <bedirhanmeyer@gmail.com>
2018-10-26 11:22:06 +0300

    Okul Kartları Kod bölümü bitti.Dizayn işlemlerine geçilecek.

--- OgrenciTakip.BLL/General/OkulBLL.cs
+++ OgrenciTakip.BLL/General/OkulBLL.cs
@@ -1,8 +1,11 @@
 ﻿using OgrenciTakip.BLL.Base;
+using OgrenciTakip.Common.Enums;
 using OgrenciTakip.Data.Context;
+using OgrenciTakip.Model.DTO;
 using OgrenciTakip.Model.Entities;
 using OgrenciTakip.Model.Entities.Base;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Windows.Forms;
 
@@ -13,9 +16,44 @@ namespace OgrenciTakip.BLL.General
         public OkulBLL() { }
         public OkulBLL(Control ctrl) : base(ctrl) { }
 
-        public BaseEntity Single(Expression<Func<Okul,bool>> filter)
+        public BaseEntity Single(Expression<Func<Okul, bool>> filter)
         {
-            return BaseSingle(filter,)
+            return BaseSingle(filter, x => new OkulS
+            {
+                Id = x.Id,
+                Kod = x.Kod,
+                OkulAdi = x.OkulAdi,
+                IlId = x.IlId,
+                IlAdi = x.Il.IlAdi,
+                IlceId = x.IlceId,
+                IlceAdi = x.Ilce.IlceAdi,
+                Aciklama = x.Aciklama,
+                Durum = x.Durum
+            });
+        }
+        public IEnumerable<BaseEntity> List(Expression<Func<Okul, bool>> filter)
+        {
+            return BaseList(filter, x => new OkulL
+            {
+                Id = x.Id,
+                Kod = x.Kod,
+                OkulAdi = x.OkulAdi,
+                IlAdi = x.Il.IlAdi,
+                IlceAdi = x.Ilce.IlceAdi,
+                Aciklama = x.Aciklama
+            });
+        }
+        public bool Insert(BaseEntity entity)
+        {
+            return BaseInsert(entity, x => x.Kod == entity.Kod);
+        }
+        public bool Update(BaseEntity oldEntity, BaseEntity currentEntity)
+        {
+            return BaseUpdate(oldEntity, currentEntity, x => x.Kod == currentEntity.Kod);
+        }
+        public bool Delete(BaseEntity entity)
+        {
+            return BaseDelete(entity, KartTuru.Okul);
         }
     }
 }
---
+++ OgrenciTakip.Model/DTO/OkulDTO.cs
@@ -0,0 +1,23 @@
+﻿
+
+using OgrenciTakip.Model.Entities;
+using OgrenciTakip.Model.Entities.Base;
+
+namespace OgrenciTakip.Model.DTO
+{
+    //Burada attribute olacak.
+
+    public class OkulS : Okul
+    {
+        public string IlAdi { get; set; }
+        public string IlceAdi { get; set; }
+    }
+
+    public class OkulL : BaseEntity
+    {
+        public string OkulAdi { get; set; }
+        public string IlAdi { get; set; }
+        public string IlceAdi { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
